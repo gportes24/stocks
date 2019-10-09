3876066c01f3f56460a746f340942e910fa8cd63
German Portes <germanportes@Germans-MacBook-Air.local>
2019-10-08 22:57:40 -0400

    initial

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import dash
-imoport dash_html_components as html
+import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
 import plotly.graph_objs as go
