d06aac0746668d8f1664e45ba84649547324cbda
German Portes <germanportes@Germans-MacBook-Air.local>
2019-10-08 23:15:01 -0400

    initial

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import plotly.graph_objs as go
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgp.css']
 
 app= dash.Dash(__name__, external_stylesheets=external_stylesheets)
-df = pd.read_csv('https://github.com/gportes24/stocks/blob/master/sp500_1.csv')
+df = pd.read_csv(
+    'https://github.com/gportes24/stocks/blob/master/sp500_1.csv')
 
 app.layout = html.Div([
     dcc.graph(
